fa1b35beb836974ec4b8f6d7fdddf0d32cc096d1
theprogrammizz <theprogrammizz@gmail.com>
2026-03-20 13:33:39 +0530

    made some changes

--- lib/database/tables/user_settings.dart
+++
@@ -1,7 +0,0 @@
-import 'package:drift/drift.dart';
-
-class UserSettings extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text().withDefault(const Constant('User'))();
-  BoolColumn get isDark => boolean().withDefault(const Constant(false))();
-}
